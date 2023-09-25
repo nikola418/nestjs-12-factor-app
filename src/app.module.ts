7db847007c55d09e916edb2b0aeb26269e7b9494
Nikola Milovanovic <nikolam998@gmail.com>
2023-09-25 05:29:56 +0200

    XII

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -9,15 +9,24 @@ import { PrismaService } from './prisma.service';
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule implements OnModuleInit {
+export class AppModule implements OnModuleInit, OnModuleDestroy {
   constructor(
     private config: ConfigService,
     private prisma: PrismaService,
   ) {
-    console.log(config.get('NODE_ENV'));
+    this.logger.log(config.get('NODE_ENV'));
   }
 
+  private logger = new Logger(AppModule.name);
+
   async onModuleInit() {
-    console.log(await this.prisma.user.findMany());
+    this.prisma.$connect();
+    const users = await this.prisma.user.findMany();
+    this.logger.log(`Users: ${users}`);
+  }
+
+  async onModuleDestroy() {
+    this.logger.log('SHUTTING DOWN...');
+    this.prisma.$disconnect();
   }
 }
